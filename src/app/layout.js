c40c9c45ce899698fe2304bd3eba0a42af0b5c6f
SSE World <114757626+sseworld@users.noreply.github.com>
2024-06-23 03:09:22 +0000

    app complete

---
+++ src/app/layout.js
@@ -0,0 +1,36 @@
+import Navbar from "@/components/navbar/Navbar";
+import "./global.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/providers/ThemeProvider";
+import AuthProvider from "@/providers/AuthProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "SSE Blog",
+  description: "Blog Designed for Bloging and documentation",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthProvider>
+          <ThemeContextProvider>
+            <ThemeProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Navbar />
+                  {children}
+                  <Footer />
+                </div>
+              </div>
+            </ThemeProvider>
+          </ThemeContextProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
